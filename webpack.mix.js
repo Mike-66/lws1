c98ddefb3efd2f32f270e44e001ff542cb115f73
Mike-66 <mikemeyers@mein.gmx>
2021-08-13 08:43:12 +0200

    add install hints to readme.md

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,7 +18,9 @@ mix.js('resources/js/app.js', 'public/js')
         require('tailwindcss'),
         require('autoprefixer'),
     ])
-    .webpackConfig(require('./webpack.config'));
+    .webpackConfig(require('./webpack.config'),
+                    require("dotenv").config());
+
 
 if (mix.inProduction()) {
     mix.version();
